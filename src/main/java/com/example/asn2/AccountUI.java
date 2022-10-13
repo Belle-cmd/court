a7305f6cf4bd3c9f499b808f12d8081dcf1d6fa2
BelleProjects <bautistadana55@gmail.com>
2022-10-12 22:24:05 -0600

    Add image to button

--- src/main/java/com/example/asn2/AccountUI.java
+++ src/main/java/com/example/asn2/AccountUI.java
@@ -3,14 +3,23 @@ package com.example.asn2;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 
+import java.util.Objects;
+
 public class AccountUI extends BorderPane {
     public AccountUI() {
         Button backBtn = new Button();
+        ImageView leftArrowImg = new ImageView(
+                Objects.requireNonNull(getClass().getResource("/leftArrow.png")).toExternalForm());
+        leftArrowImg.setFitHeight(50);  // set the size of the image
+        leftArrowImg.setFitWidth(60);
+        backBtn.setGraphic(leftArrowImg);
         this.setTop(backBtn);
     }
 }
